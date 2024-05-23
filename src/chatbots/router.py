3cfa2eb9a9317859c1cf2f0f69f378c06cc871fb
오진영 <ojy101039@gmail.com>
2024-05-23 21:45:43 +0900

    [Fix] 챗봇 메세지 API 확인 출력 변경

--- src/chatbots/router.py
+++ src/chatbots/router.py
@@ -7,7 +7,7 @@ router = APIRouter()
 @router.post("", status_code=status.HTTP_200_OK, response_model=ChatBotResponse)
 async def send_message(request: ChatBotRequest):
     # 나중에 로그찍는걸로 바꿔도 좋을듯
-    print("[INPUT] : member_id - ", member_id, " | query - ", query)
+    print("[INPUT] : member_id - ", request.member_id, " | query - ", request.query)
     await service.save_post_md(request.member_id)
     result = await service.send_query(request.member_id, request.query)
     return {
