eeddd233e4f24a6c47dfb2a0525fb045060f7e3e
오진영 <ojy101039@gmail.com>
2024-05-23 21:35:37 +0900

    [Feat] ChatBotRequest

--- src/chatbots/schemas.py
+++ src/chatbots/schemas.py
@@ -1,6 +1,10 @@
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ChatBotResponse(BaseModel):
     message: str
+
+class ChatBotRequest(BaseModel):
+    member_id: int = Field(..., alias='memberId')
+    query: str
 
